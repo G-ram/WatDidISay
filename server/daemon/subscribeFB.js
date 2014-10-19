cd9345ba44d69dbf7a378594c2b649a1daa6d5ca
G-Ram <ggobieski@yahoo.com>
2014-10-19 10:57:40 -0400

    Working comparison

--- server/daemon/subscribeFB.js
+++ server/daemon/subscribeFB.js
@@ -57,18 +57,45 @@ function invokeWatson(rawString){
 	return analysis["content"];
 }
 function computeDifferenceOfTrees(tree1,tree2){
+	var diff = 0;
 	var string1 = tree1+"";
 	var string2 = tree2+"";
-	var numbers1 = getNumbers(string1);
-	var numbers2 = getNumbers(string2);
+	string1 = string1.replace(/children/g, "},{");
+	string2 = string2.replace(/children/g, "},{");
+	var stringArr1 = string1.split("},{");
+	var stringArr2 = string2.split("},{");
+	var keyVal1 = [];
+	var keyVal2 = [];
+	for(var i = 2; i< stringArr1.length-1;i++){
+		var temp1 = getNumbers(stringArr1[i]);
+		var temp2 = getNumbers(stringArr2[i]);
+		if(temp2 && temp1){
+			stringArr1[i] = stringArr1[i].split("\":\"");
+			stringArr1[i][1] = stringArr1[i][1].split("\",\"");
+			keyVal1[keyVal1.length] = [stringArr1[i][1][0],temp1[0]];
+			stringArr2[i] = stringArr2[i].split("\":\"");
+			stringArr2[i][1] = stringArr2[i][1].split("\",\"");
+			keyVal2[keyVal2.length] = [stringArr2[i][1][0],temp2[0]];
+		}
+	}
 	var diff = 0;
-	for(var i = 0; i < numbers1.length;i++){
-		diff += Math.abs(numbers1[i]-numbers2[i]);
+	for(var i = 0; i < keyVal1.length-1;i++){
+		for(var j = 0; j < keyVal2.length-1;j++){
+			if(keyVal1[i][0] == keyVal2[j][0]){
+				diff += Math.abs(keyVal1[i][1]-keyVal2[j][1]);
+				//console.log(keyVal1[i][1]);
+				//console.log(keyVal2[j][1]);
+				
+			}
+		}
 	}
 	console.log("DIFF: "+diff/54);
 	return diff/54;
 }
 function getNumbers(str){
 	var regex = /[+-]?\d+\.\d+/g;
-	return str.match(regex).map(function(v) { return parseFloat(v);});
+	if(str.match(regex)){
+		return str.match(regex).map(function(v) { return parseFloat(v);});
+	}
+	return null;
 }
